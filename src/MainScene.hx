412b538829ffa8f9f3f61cf9d89b866254dc8f61
Marcus Pointner <marcus.pointner@gmail.com>
2014-08-06 18:21:38 +0300

    added a super-simple animation
    as seen very similar in
    http://haxepunk.com/documentation/tutorials/haxepunk-basics/

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,9 +1,27 @@
+package;
+
 import com.haxepunk.Scene;
 
 class MainScene extends Scene
 {
-	public override function begin()
-	{
+	// defining class members	
+	private var block:com.haxepunk.Entity;
 
+	// constructor
+	public function new() {
+		super();
+	}
+
+	// methods
+	public override function begin() {
+		// add the png-image to the Entity
+		block = addGraphic(new com.haxepunk.graphics.Image("graphics/block.png"));
+	}
+
+	public override function update() {
+
+		block.moveBy(2, 1);
+
+		super.update();
 	}
 }
